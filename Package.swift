5242a35b1a9cce00bbdc0ca860b06b6780bf0da4
keyvchan <keyvchan@gmail.com>
2020-06-01 23:06:14 +0800

    [setup]: Setup project layout and add dependences.

--- Package.swift
+++ Package.swift
@@ -9,20 +9,66 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "swift-proxy",
-            targets: ["swift-proxy"]),
+            targets: ["swift-proxy"]
+        ),
+        .executable(
+            name: "SocksServer",
+            targets: ["SocksServer"]
+        ),
+        .executable(
+            name: "SocksClient",
+            targets: ["SocksClient"]
+        ),
+        .executable(
+            name: "HTTPServer",
+            targets: ["HTTPServer"]
+        ),
+        .executable(
+            name: "HTTPClient",
+            targets: ["HTTPClient"]
+        ),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-nio", from: "2.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "swift-proxy",
-            dependencies: []),
+            dependencies: [
+                .product(name: "NIO", package: "swift-nio"),
+            ]
+        ),
         .testTarget(
             name: "swift-proxyTests",
-            dependencies: ["swift-proxy"]),
+            dependencies: ["swift-proxy"]
+        ),
+
+        .target(
+            name: "SocksServer",
+            dependencies: [
+                .product(name: "NIO", package: "swift-nio"),
+            ]
+        ),
+        .target(
+            name: "SocksClient",
+            dependencies: [
+                .product(name: "NIO", package: "swift-nio"),
+            ]
+        ),
+        .target(
+            name: "HTTPServer",
+            dependencies: [
+                .product(name: "NIO", package: "swift-nio"),
+            ]
+        ),
+        .target(
+            name: "HTTPClient",
+            dependencies: [
+                .product(name: "NIO", package: "swift-nio"),
+            ]
+        ),
     ]
 )
